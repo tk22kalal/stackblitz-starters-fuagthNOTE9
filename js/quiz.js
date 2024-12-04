277d3b0485ef61ee990e7a17f6709abe1dd23663
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-12-05 01:25:13 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -66,7 +66,7 @@ export class Quiz {
         }
     }
 
-    async getLearningObjectives(question, answer, correctIndex, explanation,) {
+    async getLearningObjectives(question, answer, explanation,) {
         const prompt = `
         For this ${this.difficulty.toLowerCase()} level medical question:
         Question: "${question}"
